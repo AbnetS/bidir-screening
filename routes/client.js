201c89ccb9774a300f6ebb7634b415500539a1cc
Tony Mutai <tonnimutai@gmail.com>
2017-11-10 11:55:36 +0300

    add apidoc comments and model updates

--- routes/client.js
+++ routes/client.js
@@ -17,69 +17,100 @@ var router  = Router();
  * @apiName CreateClient
  * @apiGroup Client
  *
- * @apiDescription Create new Client
+ * @apiDescription Create new Client. Request should be sent in _multipart/form-data_
  *
- * @apiParam {String} signup_type Signup type either social or basic or anon
- * @apiParam {String} picture Profile Picture image
- * @apiParam {String} full_name Full Names
+ * @apiParam {String} first_name First Name
+ * @apiParam {String} last_name Last Name
+ * @apiParam {String} grandfather_name Grandfather's Name
+ * @apiParam {String} gender Gender
+ * @apiParam {String} national_id_no National Id number
+ * @apiParam {String} national_id_card National ID Card Url
+ * @apiParam {String} date_of_birth Date of Birth
+ * @apiParam {String} civil_status Civil Status - Single,Married,Divorced,Widow,Widower
+ * @apiParam {String} woreda Woreda
+ * @apiParam {String} kebele Kebele
+ * @apiParam {String} house_no House No
+ * @apiParam {String} [spouse] Spouse
+ * @apiParam {String} [spouse.first_name] Spouse First Name
+ * @apiParam {String} [spouse.last_name] Spouse Last Name
+ * @apiParam {String} [spouse.grandfather_name] Spouse Grandfather's Name
+ * @apiParam {String} [spouse.national_id_no] Spouse National Id number
  * @apiParam {String} [email] Email Address
- * @apiParam {String} [phone] Phone Number
- * @apiParam {String} clientname Clientname either the phone_number or email, if Anonymous, app should create a unique one
- * @apiParam {String} [password] Password
- * @apiParam {String} [age] Age
- * @apiParam {String} [city] City
- * @apiParam {String} [Country] Country
- * @apiParam {String} gender Male or Female
- * @apiParam {String} [facebook] Facebook Link
- * @apiParam {String} [google] Instagram Link
+ * @apiParam {String} phone Phone Number
+ * @apiParam {Number} household_members_count Household Members Count
+ * @apiParam {String} branch Branch Client is being registered for
+ * @apiParam {String} created_by Officer Account registering this
  *
  * @apiParamExample Request Example:
  *  {
- *    picture: "https://fb.cdn.ugusgu.us./client/285475474224/profile.png",
- *    full_name: "Mary Jane",
+ *    first_name: "Mary",
+ *    last_name: "Jane",
+ *    grandfather_name: "John Doe",
+ *    national_id_no: "242535353",
+ *    national_id_card: "<SCANNED_IMAGE_OBJECT>",
+ *    date_of_birth: "'1988-11-10T00:00:00.000Z",
+ *    civil_status: "single", 
+ *    woreda: "Woreda",
+ *    kebele: "kebele",
+ *    house_no: "House Apartments, 4th Floor, F4"
  *    email: "mary.jane@gmail.com",
- *    clientname: "mary.jane@gmail.com",
- *    city: "Nairobi",
- *    country: "Kenya",
  *    gender: "Female",
- *    facebook: "https://facebook.com/clients/mary.jane",
- *    signup_type: "social"
+ *    household_members_count: 1,
+ *    branch : "556e1174a8952c9521286a60",
+ *    created_by : "556e1174a8952c9521286a60"
  *  }
  *
  * @apiSuccess {String} _id client id
- * @apiSuccess {Object} player Player Data
- * @apiSuccess {Boolean} is_active Activeness
- * @apiSuccess {String} role Client Role
- * @apiSuccess {String} realm Client Realm
- * @apiSuccess {String} last_login Last Login Time Stamp
+ * @apiSuccess {String} first_name First Name
+ * @apiSuccess {String} last_name Last Name
+ * @apiSuccess {String} grandfather_name Grandfather's Name
+ * @apiSuccess {String} gender Gender
+ * @apiSuccess {String} national_id_no National Id number
+ * @apiSuccess {String} national_id_card National ID Card Url
+ * @apiSuccess {String} date_of_birth Date of Birth
+ * @apiSuccess {String} civil_status Civil Status - Single,Married,Divorced,Widow,Widower
+ * @apiSuccess {String} woreda Woreda
+ * @apiSuccess {String} kebele Kebele
+ * @apiSuccess {String} house_no House No
+ * @apiSuccess {String} [spouse] Spouse
+ * @apiSuccess {String} [spouse.first_name] Spouse First Name
+ * @apiSuccess {String} [spouse.last_name] Spouse Last Name
+ * @apiSuccess {String} [spouse.grandfather_name] Spouse Grandfather's Name
+ * @apiSuccess {String} [spouse.national_id_no] Spouse National Id number
+ * @apiSuccess {String} [email] Email Address
+ * @apiSuccess {String} phone Phone Number
+ * @apiSuccess {Number} household_members_count Household Members Count
+ * @apiSuccess {Object} branch Branch Client is being registered for
+ * @apiSuccess {Object} created_by Officer Account registering this
  *
  * @apiSuccessExample Response Example:
  *  {
- *    _id : "556e1174a8952c9521286a60"
- *    is_active: true,
- *    clientname: "mary.jane@gmail.com",
- *    last_login: '2017-03-16T10:50:52.305Z',
- *    role: "player",
- *    realm: "client",
- *    player: {
- *      _id : "556e1174a8952c9521286a60",
- *      client : "556e1174a8952c9521286a60",
- *      device : "556e1174a8952c9521286a60",
- *      preferences : "556e1174a8952c9521286a60",
- *      score_board : "556e1174a8952c9521286a60",
- *      friends : "556e1174a8952c9521286a60",
- *      facebook: "https://facebook.com/clients/mary.jane"
- *      full_name: "Mary Jane",
- *      phone: "",
- *      country: "Kenya",
- *      city: "Nairobi",
- *      email: "mary.jane@gmail.com",
- *      ...
+ *    _id : "556e1174a8952c9521286a60",
+ *    national_id_card: "https://fb.cdn.ugusgu.us./client/285475474224.png",
+ *    first_name: "Mary",
+ *    last_name: "Jane",
+ *    grandfather_name: "John Doe",
+ *    national_id_no: "242535353",
+ *    date_of_birth: "'1988-11-10T00:00:00.000Z",
+ *    civil_status: "single", 
+ *    woreda: "Woreda",
+ *    kebele: "kebele",
+ *    house_no: "House Apartments, 4th Floor, F4"
+ *    email: "mary.jane@gmail.com",
+ *    gender: "Female",
+ *    household_members_count: 1,
+ *    branch: {
+ *		 _id : "556e1174a8952c9521286a60",
+ *       ....
+ *    },
+ *    created_by: {
+ *		 _id : "556e1174a8952c9521286a60",
+ *       ....
  *    }
  *  }
  *
  */
-router.post('/create', acl(['admin', 'officer']), clientController.create);
+router.post('/create', acl(['*']), clientController.create);
 
 
 /**
@@ -93,42 +124,59 @@ router.post('/create', acl(['admin', 'officer']), clientController.create);
  * and `per_page=<RESULTS_PER_PAGE>`.
  *
  * @apiSuccess {String} _id client id
- * @apiSuccess {Object} player Player Data
- * @apiSuccess {Boolean} is_active Activeness
- * @apiSuccess {String} role Client Role
- * @apiSuccess {String} realm Client Realm
- * @apiSuccess {String} last_login Last Login Time Stamp
+ * @apiSuccess {String} first_name First Name
+ * @apiSuccess {String} last_name Last Name
+ * @apiSuccess {String} grandfather_name Grandfather's Name
+ * @apiSuccess {String} gender Gender
+ * @apiSuccess {String} national_id_no National Id number
+ * @apiSuccess {String} national_id_card National ID Card Url
+ * @apiSuccess {String} date_of_birth Date of Birth
+ * @apiSuccess {String} civil_status Civil Status - Single,Married,Divorced,Widow,Widower
+ * @apiSuccess {String} woreda Woreda
+ * @apiSuccess {String} kebele Kebele
+ * @apiSuccess {String} house_no House No
+ * @apiSuccess {String} [spouse] Spouse
+ * @apiSuccess {String} [spouse.first_name] Spouse First Name
+ * @apiSuccess {String} [spouse.last_name] Spouse Last Name
+ * @apiSuccess {String} [spouse.grandfather_name] Spouse Grandfather's Name
+ * @apiSuccess {String} [spouse.national_id_no] Spouse National Id number
+ * @apiSuccess {String} [email] Email Address
+ * @apiSuccess {String} phone Phone Number
+ * @apiSuccess {Number} household_members_count Household Members Count
+ * @apiSuccess {Object} branch Branch Client is being registered for
+ * @apiSuccess {Object} created_by Officer Account registering this
  *
  * @apiSuccessExample Response Example:
  *  {
  *    "total_pages": 1,
  *    "total_docs_count": 0,
  *    "docs": [{
- *      _id : "556e1174a8952c9521286a60"
- *      is_active: true,
- *      clientname: "mary.jane@gmail.com",
- *      last_login: '2017-03-16T10:50:52.305Z',
- *      role: "player",
- *      realm: "client",
- *      player: {
- *        _id : "556e1174a8952c9521286a60",
- *        client : "556e1174a8952c9521286a60",
- *        device : "556e1174a8952c9521286a60",
- *        preferences : "556e1174a8952c9521286a60",
- *        score_board : "556e1174a8952c9521286a60",
- *        friends : "556e1174a8952c9521286a60",
- *        facebook: "https://facebook.com/clients/mary.jane"
- *        full_name: "Mary Jane",
- *        phone: "",
- *        country: "Kenya",
- *        city: "Nairobi",
- *        email: "mary.jane@gmail.com",
- *        ...
- *      }
+ *    _id : "556e1174a8952c9521286a60",
+ *    national_id_card: "https://fb.cdn.ugusgu.us./client/285475474224.png",
+ *    first_name: "Mary",
+ *    last_name: "Jane",
+ *    grandfather_name: "John Doe",
+ *    national_id_no: "242535353",
+ *    date_of_birth: "'1988-11-10T00:00:00.000Z",
+ *    civil_status: "single", 
+ *    woreda: "Woreda",
+ *    kebele: "kebele",
+ *    house_no: "House Apartments, 4th Floor, F4"
+ *    email: "mary.jane@gmail.com",
+ *    gender: "Female",
+ *    household_members_count: 1,
+ *    branch: {
+ *		 _id : "556e1174a8952c9521286a60",
+ *       ....
+ *    },
+ *    created_by: {
+ *		 _id : "556e1174a8952c9521286a60",
+ *       ....
+ *    }
  *    }]
  *  }
  */
-router.get('/paginate', acl(['admin']), clientController.fetchAllByPagination);
+router.get('/paginate', acl(['*']), clientController.fetchAllByPagination);
 
 /**
  * @api {get} /screening/clients/:id Get Client Client
@@ -139,34 +187,51 @@ router.get('/paginate', acl(['admin']), clientController.fetchAllByPagination);
  * @apiDescription Get a user client with the given id
  *
  * @apiSuccess {String} _id client id
- * @apiSuccess {Object} player Player Data
- * @apiSuccess {Boolean} is_active Activeness
- * @apiSuccess {String} role Client Role
- * @apiSuccess {String} realm Client Realm
- * @apiSuccess {String} last_login Last Login Time Stamp
+ * @apiSuccess {String} first_name First Name
+ * @apiSuccess {String} last_name Last Name
+ * @apiSuccess {String} grandfather_name Grandfather's Name
+ * @apiSuccess {String} gender Gender
+ * @apiSuccess {String} national_id_no National Id number
+ * @apiSuccess {String} national_id_card National ID Card Url
+ * @apiSuccess {String} date_of_birth Date of Birth
+ * @apiSuccess {String} civil_status Civil Status - Single,Married,Divorced,Widow,Widower
+ * @apiSuccess {String} woreda Woreda
+ * @apiSuccess {String} kebele Kebele
+ * @apiSuccess {String} house_no House No
+ * @apiSuccess {String} [spouse] Spouse
+ * @apiSuccess {String} [spouse.first_name] Spouse First Name
+ * @apiSuccess {String} [spouse.last_name] Spouse Last Name
+ * @apiSuccess {String} [spouse.grandfather_name] Spouse Grandfather's Name
+ * @apiSuccess {String} [spouse.national_id_no] Spouse National Id number
+ * @apiSuccess {String} [email] Email Address
+ * @apiSuccess {String} phone Phone Number
+ * @apiSuccess {Number} household_members_count Household Members Count
+ * @apiSuccess {Object} branch Branch Client is being registered for
+ * @apiSuccess {Object} created_by Officer Account registering this
  *
  * @apiSuccessExample Response Example:
  *  {
- *    _id : "556e1174a8952c9521286a60"
- *    is_active: true,
- *    clientname: "mary.jane@gmail.com",
- *    last_login: '2017-03-16T10:50:52.305Z',
- *    role: "player",
- *    realm: "client",
- *    player: {
- *      _id : "556e1174a8952c9521286a60",
- *      client : "556e1174a8952c9521286a60",
- *      device : "556e1174a8952c9521286a60",
- *      preferences : "556e1174a8952c9521286a60",
- *      score_board : "556e1174a8952c9521286a60",
- *      friends : "556e1174a8952c9521286a60",
- *      facebook: "https://facebook.com/clients/mary.jane"
- *      full_name: "Mary Jane",
- *      phone: "",
- *      country: "Kenya",
- *      city: "Nairobi",
- *      email: "mary.jane@gmail.com",
- *      ...
+ *    _id : "556e1174a8952c9521286a60",
+ *    national_id_card: "https://fb.cdn.ugusgu.us./client/285475474224.png",
+ *    first_name: "Mary",
+ *    last_name: "Jane",
+ *    grandfather_name: "John Doe",
+ *    national_id_no: "242535353",
+ *    date_of_birth: "'1988-11-10T00:00:00.000Z",
+ *    civil_status: "single", 
+ *    woreda: "Woreda",
+ *    kebele: "kebele",
+ *    house_no: "House Apartments, 4th Floor, F4"
+ *    email: "mary.jane@gmail.com",
+ *    gender: "Female",
+ *    household_members_count: 1,
+ *    branch: {
+ *		 _id : "556e1174a8952c9521286a60",
+ *       ....
+ *    },
+ *    created_by: {
+ *		 _id : "556e1174a8952c9521286a60",
+ *       ....
  *    }
  *  }
  *
@@ -190,34 +255,51 @@ router.get('/:id', acl(['*']), clientController.fetchOne);
  * }
  *
  * @apiSuccess {String} _id client id
- * @apiSuccess {Object} player Player Data
- * @apiSuccess {Boolean} is_active Activeness
- * @apiSuccess {String} role Client Role
- * @apiSuccess {String} realm Client Realm
- * @apiSuccess {String} last_login Last Login Time Stamp
+ * @apiSuccess {String} first_name First Name
+ * @apiSuccess {String} last_name Last Name
+ * @apiSuccess {String} grandfather_name Grandfather's Name
+ * @apiSuccess {String} gender Gender
+ * @apiSuccess {String} national_id_no National Id number
+ * @apiSuccess {String} national_id_card National ID Card Url
+ * @apiSuccess {String} date_of_birth Date of Birth
+ * @apiSuccess {String} civil_status Civil Status - Single,Married,Divorced,Widow,Widower
+ * @apiSuccess {String} woreda Woreda
+ * @apiSuccess {String} kebele Kebele
+ * @apiSuccess {String} house_no House No
+ * @apiSuccess {String} [spouse] Spouse
+ * @apiSuccess {String} [spouse.first_name] Spouse First Name
+ * @apiSuccess {String} [spouse.last_name] Spouse Last Name
+ * @apiSuccess {String} [spouse.grandfather_name] Spouse Grandfather's Name
+ * @apiSuccess {String} [spouse.national_id_no] Spouse National Id number
+ * @apiSuccess {String} [email] Email Address
+ * @apiSuccess {String} phone Phone Number
+ * @apiSuccess {Number} household_members_count Household Members Count
+ * @apiSuccess {Object} branch Branch Client is being registered for
+ * @apiSuccess {Object} created_by Officer Account registering this
  *
  * @apiSuccessExample Response Example:
  *  {
- *    _id : "556e1174a8952c9521286a60"
- *    is_active: true,
- *    clientname: "mary.jane@gmail.com",
- *    last_login: '2017-03-16T10:50:52.305Z',
- *    role: "player",
- *    realm: "client",
- *    player: {
- *      _id : "556e1174a8952c9521286a60",
- *      client : "556e1174a8952c9521286a60",
- *      device : "556e1174a8952c9521286a60",
- *      preferences : "556e1174a8952c9521286a60",
- *      score_board : "556e1174a8952c9521286a60",
- *      friends : "556e1174a8952c9521286a60",
- *      facebook: "https://facebook.com/clients/mary.jane"
- *      full_name: "Mary Jane",
- *      phone: "",
- *      country: "Kenya",
- *      city: "Nairobi",
- *      email: "mary.jane@gmail.com",
- *      ...
+ *    _id : "556e1174a8952c9521286a60",
+ *    national_id_card: "https://fb.cdn.ugusgu.us./client/285475474224.png",
+ *    first_name: "Mary",
+ *    last_name: "Jane",
+ *    grandfather_name: "John Doe",
+ *    national_id_no: "242535353",
+ *    date_of_birth: "'1988-11-10T00:00:00.000Z",
+ *    civil_status: "single", 
+ *    woreda: "Woreda",
+ *    kebele: "kebele",
+ *    house_no: "House Apartments, 4th Floor, F4"
+ *    email: "mary.jane@gmail.com",
+ *    gender: "Female",
+ *    household_members_count: 1,
+ *    branch: {
+ *		 _id : "556e1174a8952c9521286a60",
+ *       ....
+ *    },
+ *    created_by: {
+ *		 _id : "556e1174a8952c9521286a60",
+ *       ....
  *    }
  *  }
  */
