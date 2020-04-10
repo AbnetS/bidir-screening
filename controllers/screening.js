54dcb999d41a71c2c4e046bb05296382c0348e8d
Abnet Shimeles <abutihere@gmail.com>
2020-04-10 16:52:04 +0300

    Make /latest return unzipped content for testing

--- controllers/screening.js
+++ controllers/screening.js
@@ -696,12 +696,12 @@ exports.fetchLatest = function* fetchLatestScreenings(next) {
     let compressedResult = yield Compressor.compress(screenings);
 
     
-    this.body = compressedResult;
-    this.set("Content-Type", "application/json")
-    this.set("Content-Encoding", "gzip")
+    // this.body = compressedResult;
+    // this.set("Content-Type", "application/json; charset=utf-8");
+    // this.set("Content-Encoding", "gzip");
     
 
-    //this.body = screenings;
+    this.body = screenings;
     
   } catch(ex) {
     return this.throw(new CustomError({
